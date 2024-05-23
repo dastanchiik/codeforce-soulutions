1b889463361f2777c053104837a7d177ed71a84b
Administrator <turdubaeveldar6@gmail.com>
2024-05-23 13:26:01 +0300

    Eldos

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,10 @@ public class Main {
         String up = first.toUpperCase();
         String low = second.toLowerCase();
         String sum = up+low;
+
+
+        System.out.println();
         return sum;
+
     }
 }
