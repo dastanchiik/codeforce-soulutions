352aedff8bf7c31aaf1540a49f1f0abff6bde11d
CompMir.kg <dosyafree32@gmail.com>
2024-05-23 15:52:37 +0600

    qwerty

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(upAndLow(scanner.next()));
+    }
+    public static String upAndLow(String word){
+        int length = word.length()/2;
+        String first = word.substring(0,length);
+        String second = word.substring(length);
+        String up = first.toUpperCase();
+        String low = second.toLowerCase();
+        String sum = up+low;
+        return sum;
+    }
+}
