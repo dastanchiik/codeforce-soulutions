cc51b3b4ae4b1cd848b333a5bd8bb3901d9458d8
CompMir.kg <dosyafree32@gmail.com>
2024-05-29 15:32:10 +0600

    just practice

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        String[] str = new String[a+1];
-        for (int i = 0; i <= a; i++) {
-            str[i] = scanner.nextLine();
-        }
-        for (String s:str){
-            if (s.length()>=11){
-            int length = s.length()-2;
-            String first = s.substring(0,1);
-            String last = s.substring(length+1);
-            String finish = first+length+last;
-            System.out.println(finish);
-            }else {
-                System.out.println(s);
+        int count = 0;
+        for (int i = 0; i < a; i++) {
+            String number = scanner.next();
+            int countLine = 0;
+            for (int j = 0; j < number.length(); j++) {
+                char[] c = number.toCharArray();
+                for (char q : c) {
+                    if (q == '1') {
+                        countLine++;
+                    }
+                    if (countLine > 3){
+                        countLine = 0;
+                    }
+                }
+            }
+            if (countLine >= 2) {
+                count++;
             }
         }
+        System.out.println(count);
     }
 }
--- src/Person.java
+++
@@ -1,4 +0,0 @@
-public class Person {
-    String name;
-    int age;
-}
