b5b9efaf4831aa91ff1574e7c5df682a0372e4f0
CompMir.kg <dosyafree32@gmail.com>
2024-05-30 11:28:37 +0600

    and one more task is done

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        scanner.nextLine();
+        int[] array = new int[a];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
         int count = 0;
-        for (int i = 0; i < a; i++) {
-            String numbers = scanner.nextLine();
-            int num = Integer.parseInt(numbers.replace(" ", ""));
-            int b = (num / 100) % 10;
-            int c = (num / 10) % 10;
-            int d = num % 10;
-            int sum = b + c + d;
-            if (sum >= 2) {
+        int num = scanner.nextInt();
+        for (int q:array){
+            if (q == num){
                 count++;
             }
         }
