c420d92a637ca8e3a51c7dc2d5f27a8070761a33
CompMir.kg <dosyafree32@gmail.com>
2024-06-04 15:41:58 +0600

    and one more task is done

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,54 @@
-import java.util.Arrays;
-import java.util.OptionalInt;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Map<Integer, Person> personMap = new HashMap<>();
         int a = scanner.nextInt();
-        int[] array = new int[a];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = scanner.nextInt();
+        int b = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < a; i++) {
+            Integer st = scanner.nextInt();
+            scanner.nextLine();
+            String str = scanner.nextLine();
+            String str1 = scanner.nextLine();
+            Person person = new Person(str, str1);
+            personMap.put(st, person);
         }
-        OptionalInt max = Arrays.stream(array).max();
-        OptionalInt min = Arrays.stream(array).min();
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == max.getAsInt()) {
-                array[i] = min.getAsInt();
-            }
+        for (int i = 0; i < b; i++) {
+            Integer num = scanner.nextInt();
+            System.out.println(personMap.get(num));
         }
-        for (int r:array){
-            System.out.print(r+" ");
-        }
+    }
+}
+
+class Person {
+    private String fio;
+    private String address;
+
+    public Person(String fio, String address) {
+        this.fio = fio;
+        this.address = address;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return fio + " " + '(' + address + ')';
     }
 }
