f47bbe385e226832b6af765ba3c3cfee69f5043e
CompMir.kg <dosyafree32@gmail.com>
2024-06-11 16:29:45 +0600

    and one more task is not done

---
+++ src/Dog.java
@@ -0,0 +1,8 @@
+public class Dog extends Animal{
+    private String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[scanner.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
 
+    public static int[] selectionSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int maxIndex = i;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            System.out.println(maxIndex);
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
     }
 }
