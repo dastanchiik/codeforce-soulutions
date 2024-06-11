c4a1dc99ab8afd5a4192da359ca8304711608feb
CompMir.kg <dosyafree32@gmail.com>
2024-06-11 16:29:45 +0600

    and one more task is not done
    (cherry picked from commit f47bbe385e226832b6af765ba3c3cfee69f5043e)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Create Dog -> 1\nFind Dog -> 2\nGet All Dogs -> 3");
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int[] arr = new int[scanner.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
 
+    public static int[] selectionSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int maxIndex = i;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            System.out.println(maxIndex);
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
     }
 }
