9ae2c2013a771891725613955e4e76e97d305797
CompMir.kg <dosyafree32@gmail.com>
2024-06-12 17:20:50 +0600

    and one more task is not done

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,38 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] arr = new int[scanner.nextInt()];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        System.out.println(Arrays.toString(selectionSort(arr)));
-    }
+    private static int[] a = new int[0];
 
-    public static int[] selectionSort(int[] arr) {
-        int[] array = new int[arr.length];
-        for (int i = arr.length - 1; i > 0; i--) {
-            int maxIndex = i;
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[maxIndex]) {
-                    maxIndex = j;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        a = new int[c];
+        for (int i = 0; i < c; i++) {
+            a[i] = sc.nextInt();
+        }
+    }
+    public static void method(int l,int r){
+        if (l<r){
+            int x = a[(l+r)/2];
+            int i = l;
+            int j = r;
+            while (i <= j){
+                while (a[i]<x){
+                    i++;
+                }
+                while (a[j]> x){
+                    j--;
                 }
             }
-            array[i] = maxIndex;
-            int temp = arr[maxIndex];
-            arr[maxIndex] = arr[i];
-            arr[i] = temp;
+            i++;
+            j++;
+            if (i <= j){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
         }
-        return array;
+        method(l,r);
+        method(l,r);
     }
 }
-//40 30 20 40 20
-//
