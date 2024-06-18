776d47c39f2a075d356660cf010b13af5da5dfdb
CompMir.kg <dosyafree32@gmail.com>
2024-06-18 17:53:24 +0600

    MKAD

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int a = k/n;
-        int apple = k%n;
-        int p = 0;
-        int sum = k-(a*n);
-        System.out.println(a);
-        System.out.println(sum);
-        if (apple == 0){
-            p = 0;
-        }else {
-            p = n - apple;
-        }
-        System.out.println(p);
+        Scanner scanner = new Scanner(System.in);
+        int v = scanner.nextInt();
+        int t = scanner.nextInt();
+        final int length = 109;
+        int distanceTraveled = v * t;
+        int finalPosition = (distanceTraveled % length + length) % length + 1;
+        System.out.println(finalPosition);
     }
 }
