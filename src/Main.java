b3de430ef0c44e33b2d018d24950ce0e6fd19cb9
CompMir.kg <dosyafree32@gmail.com>
2024-06-19 16:26:15 +0600

    task is not done

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int v = scanner.nextInt();
-        int t = scanner.nextInt();
-        final int length = 109;
-        int distanceTraveled = v * t;
-        int finalPosition = (distanceTraveled % length + length) % length + 1;
-        System.out.println(finalPosition);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int x = 3;
+        int y = 5;
+        int z = 12;
+        long sum = 0;
+        sum += (long) a * x;
+        sum += (long) b * y;
+        sum += (long) c * z;
+        System.out.println(sum);
     }
 }
