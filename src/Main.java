f5f5846c8ff45a0d424b622a4cda3f65cd909970
CompMir.kg <dosyafree32@gmail.com>
2024-06-21 18:42:47 +0600

    and one task is done

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int first = a/1000;
-        int second = a%1000;
-        int ff = first%10;
-        int fs = (first/10)%10;
-        int ft = (first/100)%10;
-        int sf =  second%10;
-        int ss = (second/10)%10;
-        int st = (second/100)%10;
-        int sum1 = ff+fs+ft;
-        int sum2 = sf+ss+st;
-        if (sum1==sum2){
-            System.out.println("YES");
-        }else {
-            System.out.println("NO");
-        }
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int max1 = Math.max(a,b);
+        int max2 = Math.max(max1,c);
+        int min1 = Math.min(a,b);
+        int min2 = Math.min(c,min1);
+        System.out.println(max2-min2);
     }
 }
