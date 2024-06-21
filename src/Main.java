d31bdafd6cddbe14dfeb56309581a7aa9bd260c6
CompMir.kg <dosyafree32@gmail.com>
2024-06-21 18:17:13 +0600

    it's task for students

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int x = 3;
-        int y = 5;
-        int z = 12;
-        long sum = 0;
-        sum += (long) a * x;
-        sum += (long) b * y;
-        sum += (long) c * z;
-        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 --> Создать Акулу\n2 --> создать Кита\n3 --> Посчитать Акулы\n4 --> Посчитать Китов");
+        int a = sc.nextInt();
+        Animal[] animals = new Animal[100];
+        int countShark = 0;
+        int countWhale = 0;
+        for (int i = 0; true; i++) {
+            if (a == 1){
+                Shark shark = new Shark(sc.next(), sc.nextInt(), sc.nextInt());
+                animals[i] = shark;
+                System.out.println("1 --> Создать Акулу\n2 --> создать Кита\n3 --> Посчитать Акулы\n4 --> Посчитать Китов");
+                a = sc.nextInt();
+            } else if (a == 2) {
+
+                Whale whale = new Whale(sc.next(), sc.nextInt(), sc.nextInt());
+                animals[i] = whale;
+                System.out.println("1 --> Создать Акулу\n2 --> создать Кита\n3 --> Посчитать Акулы\n4 --> Посчитать Китов");
+                a = sc.nextInt();
+            } else if (a == 3) {
+                for (Animal animal:animals){
+                    if (animal instanceof Shark){
+                        countShark++;
+                    }
+                }
+                System.out.println(countShark);
+                countShark = 0;
+                System.out.println("1 --> Создать Акулу\n2 --> создать Кита\n3 --> Посчитать Акулы\n4 --> Посчитать Китов");
+                a = sc.nextInt();
+            } else if (a == 4) {
+                for (Animal animal:animals){
+                    if (animal instanceof Whale){
+                        countWhale++;
+                    }
+                }
+                System.out.println(countWhale);
+                countWhale = 0;
+                System.out.println("1 --> Создать Акулу\n2 --> создать Кита\n3 --> Посчитать Акулы\n4 --> Посчитать Китов");
+                a = sc.nextInt();
+            }
+        }
+
     }
 }
--- src/Turtle.java
+++
@@ -1,6 +0,0 @@
-public class Turtle extends Animal{
-
-    public Turtle(String name, int age) {
-        super(name, age);
-    }
-}
