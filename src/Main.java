5dbdbaeb3cf004b5db9b46a92f95be71dddd069f
CompMir.kg <dosyafree32@gmail.com>
2024-06-21 19:16:50 +0600

    and one task is done(tort)

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int mod1 = a%10;
-        int mod2 = (a/10)%10;
-        int mod3 = (a/100)%10;
-        int mod4 = (a/1000)%10;
-        if (mod1 == mod4 && mod2 == mod3){
-            System.out.println("YES");
-        }else {
-            System.out.println("NO");
+        method(a);
+    }
+    public static void method(int a){
+        if (a <= 1) {
+            System.out.println(0);
+            return;
         }
+            if (a % 2 == 0) {
+                System.out.println(a / 2);
+
+            } else if (a % 2 == 1) {
+                System.out.println(a);
+            }
     }
 }
