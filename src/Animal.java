0801e919f28911f5c0bdf03210aee285167a6bee
CompMir.kg <dosyafree32@gmail.com>
2024-06-24 17:39:26 +0600

    just one more task for students

--- src/Animal.java
+++ src/Animal.java
@@ -1,34 +1,11 @@
-
-public class Animal {
+public abstract class Animal {
     private String name;
-    private int age;
 
-    public Animal(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 
-    public String getName() {
-        return name;
-    }
+    public abstract void voice();
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public abstract void eat();
+    public void run(){
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Animal{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
     }
 }
---
+++ src/Dog.java
@@ -0,0 +1,18 @@
+public class Dog extends Animal{
+
+    private String[] names;
+    @Override
+    public void voice() {
+        System.out.println("Gav - Gav");
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
