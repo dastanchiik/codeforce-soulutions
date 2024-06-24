25e5fd6791f268a642ee46f06dcab08262a01ea0
CompMir.kg <dosyafree32@gmail.com>
2024-06-24 15:40:31 +0600

    and one task is done(tort)

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        method(a);
-    }
-    public static void method(int a){
-        if (a <= 1) {
-            System.out.println(0);
-            return;
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int max1 = Math.max(a,b);
+        int max2 = Math.max(c,max1);
+        if (a < 94 || b < 94 || c < 94 ){
+            System.out.println("Error");
+        } else if (a > 727 || b > 727 || c > 727) {
+            System.out.println("Error");
+        }else {
+            System.out.println(max2);
         }
-            if (a % 2 == 0) {
-                System.out.println(a / 2);
-
-            } else if (a % 2 == 1) {
-                System.out.println(a);
-            }
     }
 }
