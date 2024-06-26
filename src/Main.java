8bb83184ce6995310fc4f6fe05bab25c97e36dbf
CompMir.kg <dosyafree32@gmail.com>
2024-06-26 18:53:16 +0600

    and one task is not done (chess)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int e = scanner.nextInt();
+        if (a % 2 == 1 && c % 2 == 1 && b % 2 == 0 && e % 2 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
 
     }
 }
