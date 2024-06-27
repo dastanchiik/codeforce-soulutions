5f425c5b4b96c76e056c829da857b6b7819e0f05
CompMir.kg <dosyafree32@gmail.com>
2024-06-27 22:22:38 +0600

    I'm not done this task(Algorithm first task)

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count1 = 0;
-        int count2 = 0;
-        for (int i = 0; i < 4; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            count1 += a;
-            count2 += b;
-        }
-        if (count1 > count2){
-            System.out.println(1);
-        } else if (count2 == count1) {
-            System.out.println("DRAW");
-        }else {
-            System.out.println(2);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int count = 0;
+        while (true) {
+            if (a + b == c) {
+                System.out.println("YES");
+                break;
+            } else if (a + b != c) {
+                int temp = c;
+                c = b;
+                b = temp;
+                count++;
+                if (count == 3){
+                    System.out.println("NO");
+                    break;
+                }
+            }
         }
     }
 }
