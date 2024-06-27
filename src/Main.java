d9369f6ea8b1c8746054a95a308b802e2ea4e6ce
CompMir.kg <dosyafree32@gmail.com>
2024-06-27 16:00:55 +0600

    четырехзначный полиндром

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int e = scanner.nextInt();
-        int placeFirst = 0;
-        int placeSecond = 0;
-        if (a % 2 == 1 && b % 2 == 1) {
-            placeFirst = 2;
-        } else if (a % 2 == 0 && b % 2 == 0) {
-            placeFirst = 2;
-        } else if ((a % 2 == 0 && b % 2 == 1) || (a % 2 == 1 && b % 2 == 0)) {
-            placeFirst = 1;
-        }
-        if (c % 2 == 1 && e % 2 == 1) {
-            placeSecond = 2;
-        } else if (c % 2 == 0 && e % 2 == 0) {
-            placeSecond = 2;
-        } else if ((c % 2 == 0 && e % 2 == 1) || (c % 2 == 1 && e % 2 == 0)) {
-            placeSecond = 1;
-        }
 
-        if (placeFirst == placeSecond){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int mod1 = a%10;
+        int mod2 = (a/10)%10;
+        int mod3 = (a/100)%10;
+        int mod4 = (a/1000)%10;
+        if (mod1 == mod4 && mod2 == mod3){
             System.out.println("YES");
         }else {
             System.out.println("NO");
