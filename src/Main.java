0acd1a3f0f43a8e59a657ed8d7f71f33d8d6f9b9
CompMir.kg <dosyafree32@gmail.com>
2024-07-08 15:10:18 +0600

    I'm not done this task(Algorithm first task)

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int count = 0;
-        while (true) {
-            if (a + b == c) {
-                System.out.println("YES");
-                break;
-            } else if (a + b != c) {
-                int temp = c;
-                c = b;
-                b = temp;
-                count++;
-                if (count == 3){
-                    System.out.println("NO");
-                    break;
-                }
-            }
+        int count = 1;
+        int[] arr = new int[a + 1];
+        for (int i = 0; i <= a; i++) {
+            arr[i] = i;
         }
+        for (int w : arr) {
+            count += w;
+        }
+        System.out.println(count);
     }
 }
