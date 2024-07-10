b0728a638ae52ef60fb78fcc1d5d722a506bda7d
CompMir.kg <dosyafree32@gmail.com>
2024-07-10 15:26:26 +0600

    just any lesson for students(fileWriter and fileReader)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int count = 1;
-        int[] arr = new int[a + 1];
-        for (int i = 0; i <= a; i++) {
-            arr[i] = i;
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("Example.txt");
+        writer.write(" Salem!!! ");
+        writer.write(" Dostor ");
+        writer.close();
+        FileReader reader = new FileReader("Example.txt");
+        Scanner scanner = new Scanner(reader);
+        while (scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            if (line.contains("Salem")){
+            System.out.println(line.substring(line.length()/2));
+            }
         }
-        for (int w : arr) {
-            count += w;
-        }
-        System.out.println(count);
+        reader.close();
     }
 }
