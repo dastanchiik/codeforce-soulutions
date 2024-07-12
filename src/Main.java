f40da0a3a8202d5e8ebb9a60408e50f981e0ad6f
CompMir.kg <dosyafree32@gmail.com>
2024-07-12 15:34:11 +0600

    any example for students

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int max1 = Math.max(a,b);
-        int max2 = Math.max(c,max1);
-        if (a < 94 || b < 94 || c < 94 ){
-            System.out.println("Error");
-        } else if (a > 727 || b > 727 || c > 727) {
-            System.out.println("Error");
+        int mod1 = a%10;
+        int mod2 = (a/10)%10;
+        int mod3 = (a/100)%10;
+        int mod4 = (a/1000)%10;
+        if (mod1 == mod4 && mod2 == mod3){
+            System.out.println("YES");
         }else {
-            System.out.println(max2);
+            System.out.println("NO");
         }
     }
 }
