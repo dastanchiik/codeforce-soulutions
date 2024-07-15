3255250d59cba997265647134412863690de0ba1
CompMir.kg <dosyafree32@gmail.com>
2024-07-15 21:25:24 +0600

    any example for students

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,47 @@
+import java.rmi.NotBoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Person>people = new ArrayList<>();
+        System.out.println("1 --> Create new Person\n2 --> find person by id\n3 --> get All people\n4 --> delete all people");
         int a = sc.nextInt();
-        int mod1 = a%10;
-        int mod2 = (a/10)%10;
-        int mod3 = (a/100)%10;
-        int mod4 = (a/1000)%10;
-        if (mod1 == mod4 && mod2 == mod3){
-            System.out.println("YES");
-        }else {
-            System.out.println("NO");
+        int id = 0;
+        while (true){
+            if (a==1){
+                id++;
+                System.out.print("Enter name:");
+                String name = sc.next();
+                System.out.print("Enter age:");
+                int age = sc.nextInt();
+                Person person = new Person(id,name,age);
+                people.add(person);
+                System.out.println("1 --> Create new Person\n2 --> find person by id\n3 --> get All people\n4 --> delete all people");
+                a = sc.nextInt();
+            } else if (a == 2) {
+                int b = sc.nextInt();
+                for (Person person:people){
+                    if (person.getId() == b){
+                        System.out.println(person);
+                    }
+                }
+                System.out.println("1 --> Create new Person\n2 --> find person by id\n3 --> get All people\n4 --> delete all people");
+                a = sc.nextInt();
+            } else if (a == 3) {
+                for (Person person:people){
+                    System.out.println(person);
+                }
+                System.out.println("1 --> Create new Person\n2 --> find person by id\n3 --> get All people\n4 --> delete all people");
+                a = sc.nextInt();
+            } else if (a == 4) {
+                people.clear();
+                System.out.println("data is successfully cleared!!!\n\n\n");
+                System.out.println("1 --> Create new Person\n2 --> find person by id\n3 --> get All people\n4 --> delete all people");
+                a = sc.nextInt();
+            }
         }
     }
 }
